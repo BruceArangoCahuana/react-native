8154dc903c197730318ea7d9bccdd1a312a5e7a2
BruceArangoCahuana <75152945+BruceArangoCahuana@users.noreply.github.com>
2021-09-17 23:38:45 -0500

    react-native

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,Button,Alert} from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inicio}>
+        <Text style={styles.color__blanco}>Barsand International</Text>
+        <View style={styles.contenedor__imagen}>
+          <Image 
+            source={require('./assets/doctor.png')} 
+            style={styles.imagen}
+          />
+          
+        </View>
+        <View style={styles.contenedor__btn}>
+          <Button
+              title="Ingresar"
+              style={styles.btn}
+              onPress={() => Alert.alert('Simple Button pressed')}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +30,43 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#08519c',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  inicio:{
+    width:'90%',
+    height:400,
+    backgroundColor: 'white',
+    borderRadius:20
+  },
+  color__blanco:{
+    color:'black',
+    fontSize:25,
+    textAlign:'center',
+    marginTop:25,
+   
+  },
+  contenedor__imagen:{
+    width:'95%',
+    height:200,
+    marginTop:50,
+    padding:10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contenedor__btn:{
+    width:'100%',
+    padding:10,
+    margin:'auto'
+  },
+  imagen:{
+    width:'100%',
+    height:'100%',
+    marginBottom:40
+  },
+  btn:{
+    width:'80%',
+    marginTop:50,
+  }
 });
